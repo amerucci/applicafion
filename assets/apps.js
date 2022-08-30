f016bb17ecb9fdacc01599bef27e9be709e8acd1
amerucci <a.merucci@onlineformapro.com>
2022-08-30 16:40:35 +0200

    change home backgrond with de choosen gender

--- assets/apps.js
+++ assets/apps.js
@@ -2,6 +2,9 @@
  * VARIABLE *
  ************/
 const genders = document.querySelectorAll(".genders");
+const home = document.querySelector("#home");
+let bgMale = ""
+let bgFemale = ""
 
 /*************
  * FUNCTIONS *
@@ -19,13 +22,17 @@ function fillLocalStorageGenderChoose(gender){
 
 //SHOW HOME PAGE
 genders.forEach((gender) => {
-
   gender.addEventListener("click", function () {
     fillLocalStorageGenderChoose(gender.dataset.gender)
     console.log(gender.dataset.gender);
     document.querySelector("#gender").style = "display:none;";
     document.querySelector("#home").style = "display:block";
+    if(gender.dataset.gender=="male"){
+      home.style="background:url(./img/fh.jpg)"
+    }
+    else {
+      home.style="background:url(./img/ff.jpg)"
+    }
   });
 });
 
-//SAVE GENDER CHOICE IN LOCAL STORAGE
